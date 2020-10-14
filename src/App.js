25fea91d69e98a03906c1c9d69d5c497463918b7
louisececcaldi <comacle.louise@gmail.com>
2020-10-14 14:41:29 +0200

    First commit with map function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import Contact from './components/Contact'
-
+import Contact from './components/Contact';
+import PropTypes from 'prop-types';
+import ContactList from './components/ContactList'
 
 function App() {
   return (
-    <div className='App'>
-      <Contact />
+    <div className='App'> 
+
+      <ContactList/>
+      
     </div>
   );
 }
-
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired,
+};
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import './Contact.css';
 
-const online = true;
-const Contact = () => {
+function Contact(props) {
   return (
     <div className='Contact'>
-      
-        <img
-          className='avatar'
-          src='https://randomuser.me/api/portraits/women/61.jpg'
-          alt='Lola'
-        />
-  
+      <img className='avatar' src={props.avatar} alt='Peter Palmer' />
 
       <div>
-      <h4 className='name'>Lola</h4>
-      <div className = "status"> 
-        <div className='status-online'></div>
-        <p className ="status-text">{online ? 'online' : ''}</p>
-      </div>
+        <p className='name'>{props.name}</p>
+        <div className='status'>
+          <div className={props.online ? 'status-online' : 'status-offline'} />
+          <p className='status-text'>{props.online ? 'oneline' : 'offline'}</p>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Contact;
