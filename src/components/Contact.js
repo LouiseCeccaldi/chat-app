c428b656473470e3a36b335c79e186bfdcd0c3d2
louisececcaldi <comacle.louise@gmail.com>
2020-10-18 15:45:07 +0200

    quête 5 react challenge

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      props.online,
+    };
+  }
+
+
+
+  render() {
   return (
     <div className='Contact'>
       <img className='avatar' src={props.avatar} alt='Peter Palmer' />
@@ -11,6 +21,14 @@ function Contact(props) {
         <div className='status'>
           <div className={props.online ? 'status-online' : 'status-offline'} />
           <p className='status-text'>{props.online ? 'oneline' : 'offline'}</p>
+
+          <span
+          className={this.state.online ? 'online' : 'offline'}
+              onClick={event => {
+                const noffline = !this.state.online;
+                this.setState({ fonline: offline });
+              }}
+            >&#9733;</span>
         </div>
       </div>
     </div>
